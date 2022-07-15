4ce64832e8a27d5527b0f72a6d65522bc8707619
Mizore <xvz@live.com>
2022-07-15 17:55:41 +0800

    fix

--- api/index.js
+++ api/index.js
@@ -118,6 +118,7 @@ function nodeListHandle(node_list, exclude_query, sort, user_agent) {
   const stash_feature_protocol_names = ['vless', 'hysteria', 'tuic']
   if (!/Stash/i.test(user_agent)) {
     proxies_out = proxies_out.filter(i => !stash_feature_protocol_names.includes(i.type))
+    console.log('Stash feature disabled', proxies_out)
   }
 
   proxies_out.map(i => i.name = i.name.replace(/^\[.*?\]/, ''))
